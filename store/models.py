5e8edf1fe10336ddd528f9f547ccf38615a4f26c
Maximo <maxicobeagaprc@gmail.com>
2023-01-16 12:17:43 -0300

    Agregando modelo de variaciones para los products

--- store/models.py
+++ store/models.py
@@ -21,4 +21,20 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
     
+validation_category_choice = (
+    ('color','color'),
+    ('talla', 'talla'),
+)
+
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=validation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.product
+    
     
