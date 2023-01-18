d3a1c15f5014d134e37ab98f2ac257d0edf64802
Maximo <maxicobeagaprc@gmail.com>
2023-01-18 12:02:54 -0300

    Agregamos por completo el Variation

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from store.models import Product
+from store.models import Product, Variation
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -14,6 +14,20 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
+    product_variation = []
+
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value )
+                product_variation.append(variation)
+            except:
+                pass
+
+
     try:
         cart = Cart.objects.get(cart_id = _cart_id(request))
     except Cart.DoesNotExist :
@@ -25,6 +39,11 @@ def add_cart(request, product_id):
 
     try:
         cart_item =  CartItem.objects.get(product=product, cart=cart)
+
+        if len(product_variation) > 0:
+            cart_item.variations.clear()
+            for item in product_variation:
+                cart_item.variation.add(item)
         cart_item.quantity += 1
         cart_item.save()
     except CartItem.DoesNotExist:
@@ -33,6 +52,9 @@ def add_cart(request, product_id):
             quantity = 1,
             cart= cart,
         )
+        if len(product_variation) > 0:
+            for item in product_variation:
+                cart_item.variation.add(item)
         cart_item.save()
 
     return redirect('cart')
--- store/views.py
+++ store/views.py
@@ -22,7 +22,7 @@ def store(request, category_slug=None):
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True).order_by('-create_date')
-        paginator = Paginator(products, 5)
+        paginator = Paginator(products, 2)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
