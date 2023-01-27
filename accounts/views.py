0b40820cb0e788c737e5f10b87871bc0708ec492
Maximo <maxicobeagaprc@gmail.com>
2023-01-27 11:58:34 -0300

    Control de Registro, inicio de sesion y cerrar sesion terminados

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegistrationForm
+from .models import Account
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def register(request):
+
     form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+
+            messages.success(request, 'Se registro el usuario exitososamente')
+            return redirect('register')
+
+
+
+
     context = {
         'form': form,
     }
@@ -13,8 +36,26 @@ def register(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Las credenciales son incorrectas')
+            return redirect('login')
+
+
     return render(request, 'accounts/login.html')
 
 
+@login_required(login_url='login')
 def logout(request):
-    return
+    auth.logout(request)
+    messages.success(request, 'Sesion cerrada correctamente')
+
+    return redirect('login')
